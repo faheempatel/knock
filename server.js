4fc999bf380e2a3702d6ac5f2e088cf1d00505d9
Faheem Patel <faheem@faheempatel.com>
2014-02-18 13:46:01 +0000

    Setup CORS

--- server.js
+++ server.js
@@ -1,40 +1,50 @@
 var restify = require('restify');
 
 function knock(req, res, next) {
-    if (req.body.knock === "true") {
-        json.knock = true;
-        json.response = false;
-    }
-    res.end();
+  if (req.body.knock === "true") {
+      json.knock = true;
+      json.response = false;
+  }
+  res.end();
 }
 
 function getResponse(req, res, next) {
-    res.writeHead(200, {
-        'Content-Type' : 'application/json'
-    });
-    res.write(JSON.stringify(json));
-    res.end();
+  res.writeHead(200, {
+      'Content-Type' : 'application/json'
+  });
+  res.write(JSON.stringify(json));
+  res.end();
 }
 
 function answerDoor(req, res, next) {
-    if (req.body.response === "true" && json.knock === true) {
-        json.response = true;
-        json.knock = false;
+  if (req.body.response === "true" && json.knock === true) {
+      json.response = true;
+      json.knock = false;
 
-        // Unset response
-        setTimeout(function () {
-            json.response = false;
-        }, 300000);
-    }
-    res.end();
+      // Unset response
+      setTimeout(function () {
+          json.response = false;
+      }, 300000);
+  }
+  res.end();
 }
 
 var server = restify.createServer();
 server.use(restify.bodyParser({ mapParams: false }));
 
+// Setup CORS
+restify.CORS.ALLOW_HEADERS.push('accept');
+restify.CORS.ALLOW_HEADERS.push('sid');
+restify.CORS.ALLOW_HEADERS.push('lang');
+restify.CORS.ALLOW_HEADERS.push('origin');
+restify.CORS.ALLOW_HEADERS.push('withcredentials');
+restify.CORS.ALLOW_HEADERS.push('x-requested-with');
+server.use(restify.CORS());
+
+
 var json = { 
-      'knock'    : 'false'
-    , 'response' : 'false'
+    'knock'   : 'false'
+  , 'response': 'false'
 };
 
 server.get('/:name/door/knock', getResponse);
@@ -43,5 +53,5 @@ server.post('/:name/door/knock', knock);
 server.post('/:name/door/answer', answerDoor);
 
 server.listen(8080, function() {
-    console.log('Server started!');
+  console.log('Server started!');
 });
